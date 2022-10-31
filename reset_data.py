64dbbcbeca459cc2e34688a9a0520c24693454b2
noitu <dimalego35@gmail.com>
2022-10-31 23:39:33 +0300

    day 4: jsons changed

--- reset_data.py
+++ reset_data.py
@@ -5,5 +5,5 @@ from dist.CONSTANTS import default_account, default_settings
 with open('account_data/account.json', 'w') as account:
     json.dump(default_account, account)
 
-with open('settings_data/settings.json', 'w') as settings:
+with open('settings/settings.json', 'w') as settings:
     json.dump(default_settings, settings)
