05971029ec61e9e393f08a301b839fe6bb91732f
noitu <dimalego35@gmail.com>
2022-11-02 13:14:21 +0300

    day 6: add a database

--- menuWindow.py
+++ menuWindow.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import QMainWindow, QListWidgetItem
 
@@ -19,21 +21,22 @@ class MenuPage(QMainWindow, MenuWindow):
         self.trainsView.itemClicked.connect(self.Clicked)
 
     def load(self):
-        with open('data/all_texts.txt', 'r', encoding=encoding) as f:
-            texts = f.readlines()
+        texts = self.load_from_db() # load text list from database
         titles = []
 
-        texts = list(map(str.rstrip, texts))
+        # texts = list(map(str.replace('\\n', '\n'), texts))
         self.texts = texts
 
-        for text in texts:
-            with open(f'texts/{text.rstrip()}', 'r', encoding=encoding) as f:
-                mas = f.read()
+        '''generate titles'''
 
-                title = ' '.join(mas.rstrip().split()[:2]).capitalize()
+        for text in texts:
+            mas = text
+            title = ' '.join(mas.rstrip().split()[:2]).capitalize()
             titles.append(title if title[-1] not in ',./-_;:' else title[:-1])
 
         self.titles = titles
+
+        '''put titles on listWidget'''
 
         for i, title in enumerate(titles):
             item = QListWidgetItem()
@@ -44,6 +47,15 @@ class MenuPage(QMainWindow, MenuWindow):
             item.setFont(font)
             item.setText(title)
             self.trainsView.addItem(item)
+
+    def load_from_db(self):
+        con = sqlite3.connect("data/data.db")
+        cur = con.cursor()
+        res = []
+        for item in cur.execute("""SELECT text FROM texts""").fetchall():
+            res.append(item[0])
+        con.close()
+        return res
 
     def Clicked(self, item):
         train = item.text()
--- reset_data.py
+++ reset_data.py
@@ -1,6 +1,8 @@
 import json
+import sqlite3
 
-from const.CONSTANTS import default_account, default_settings
+
+from const.CONSTANTS import default_account, default_settings, encoding
 
 with open('data/account.json', 'w') as account:
     json.dump(default_account, account)
@@ -10,3 +12,11 @@ with open('data/settings.json', 'w') as settings:
 
 with open('data/times.txt', 'w') as times:
     times.write('')
+
+con = sqlite3.connect("data/data.db")
+cur = con.cursor()
+
+with open('create_db.sql', 'r', encoding=encoding) as sql_file:
+    sql = sql_file.read()
+result = cur.executescript(sql)
+con.close()
