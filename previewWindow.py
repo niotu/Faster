081a85add3f769bf3f2ba09d28d849543acdfbc6
noitu <dimalego35@gmail.com>
2022-11-07 21:31:04 +0300

    bug fixed; add explanation_note.md

--- previewWindow.py
+++ previewWindow.py
@@ -7,7 +7,6 @@ class PreviewPage(QMainWindow, PreviewWindow):
     def __init__(self):
         super(PreviewPage, self).__init__()
 
-        self.kitten = None
         self.setupUi(self)
         self.load('name')
 
